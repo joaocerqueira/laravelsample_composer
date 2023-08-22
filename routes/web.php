d0871cf11c49bf46d19c67cab2440cf8429b8e7b
joaocerqueira <jbraga.cerq@gmail.com>
2023-08-22 16:27:15 -0300

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Default
 Route::get('/', function () {
     return view('welcome');
 });
